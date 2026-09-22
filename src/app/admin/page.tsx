aaaf9949966414a2900581ffd6e747947ae8fd3b
Jacob Kelly <jskelly2021@gmail.com>
2026-09-22 16:44:04 -0500

    linting/format check

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,9 +4,7 @@ import ActivityTable from '../../components/activity_table/activity_table';
 import Button from '../../components/button/button';
 
 export default function HomePage() {
-
   return (
-    
     <section className="mx-auto mt-16 w-full max-w-6xl px-6 pb-16 text-left text-primary-dark">
       <h2 className="mb-4 text-2xl font-bold text-primary-dark">Admin Panel</h2>
       <div className="mb-4 grid grid-cols-3 gap-4">
@@ -35,5 +33,5 @@ export default function HomePage() {
       <h2 className="mb-4 text-2xl font-bold text-primary-dark">All Activity</h2>
       <ActivityTable />
     </section>
-  )
+  );
 }
