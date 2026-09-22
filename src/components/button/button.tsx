7734327dfccb8a1622cfb2ef8406b95f5100d9ec
DanielCBruni <daniel.c.bruni@gmail.com>
2026-09-22 13:58:18 -0500

    Added button component and mockData file to be used for local testing.

---
+++ src/components/button/button.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import type { ButtonHTMLAttributes } from 'react';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+	buttonName: string;
+};
+
+export default function Button({ buttonName, style, ...props }: ButtonProps) {
+	return (
+		<button
+			{...props}
+			style={{
+				backgroundColor: 'var(--color-primary-muted)',
+				border: '1px solid var(--color-accent)',
+				borderRadius: '4px',
+				color: 'var(--color-secondary)',
+				cursor: 'pointer',
+				padding: '8px 16px',
+				...style,
+			}}>
+			{buttonName}
+		</button>
+	);
+}
